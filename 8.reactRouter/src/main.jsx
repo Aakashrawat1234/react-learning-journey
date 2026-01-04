ec43f582db7882495f2eec197c11fb931993d41c
Aakash Rawat <aakashrawat9634@gmail.com>
2026-01-04 23:04:24 +0530

    Learned ReactRouter with basic project

---
+++ 8.reactRouter/src/main.jsx
@@ -0,0 +1,47 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+
+/*
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+    {
+      path:"",
+      element:<Home/>
+    },
+     {
+      path:"about",
+      element:<About/>
+    },
+     {
+      path:"contact",
+      element:<Contact/>
+    }
+    ]
+  }
+]) */
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} >
+    <Route path="" element={<Home/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/contact" element={<Contact/>} />
+    </Route>
+    
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router}/>
+  </StrictMode>,
+)
